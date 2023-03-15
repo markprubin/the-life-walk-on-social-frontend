c95304f84b629fe999f8207c7cdf2ae5f04b8a2f
markprubin <mark.p.rubin@gmail.com>
2023-03-14 21:33:21 -0700

    task form and list margin updates

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -12,13 +12,28 @@ export function Tasks() {
     });
   };
 
+  const handleCreateTask = (params) => {
+    axios.post(`http://localhost:3000/tasks.json`, params).then((response) => {
+      console.log(response, "Creating Task.");
+    });
+  };
+
+  const handleSubmit = (task) => {
+    task.preventDefault();
+    const params = new FormData(task.target);
+    console.log("handleSubmit", params);
+    handleCreateTask(params);
+    task.target.reset();
+  };
+
   useEffect(handleViewTasks, []);
 
   return (
     <>
-      <form>
-        <div class="form-floating mb-3">
-          <input type="text" class="form-control" id="floatingInput" placeholder="Write Your Task Here" />
+      <form onSubmit={handleSubmit}>
+        <h2 class="mx-5">Let's add a task!</h2>
+        <div class="form-floating mb-3 mx-5 my-5">
+          <input name="title" type="text" class="form-control" id="floatingInput" placeholder="Write Your Task Here" />
           <label for="floatingInput">Add Task</label>
 
           <div id="taskcomplete" class="form-text">
@@ -26,21 +41,22 @@ export function Tasks() {
           </div>
         </div>
 
-        <div class="mb-3 form-check">
+        <div class="mb-3 mx-5 form-check">
           <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+
           <label class="form-check-label" for="exampleCheck1">
             Already completed?
           </label>
         </div>
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" class="btn btn-primary mx-5 my-3">
           Add Task
         </button>
       </form>
-      <div>
+      <div class="mx-5">
         <h2> Task List: </h2>
         {tasks.map((task) => (
           <div key={task.id}>
-            <h3>{task.title}</h3>
+            <h4>{task.title}</h4>
             <h5>{task.status}</h5>
           </div>
         ))}
