43f77bc3ad8da1fd5bdd5ce05391a7ae6c90bcc8
markprubin <mark.p.rubin@gmail.com>
2023-03-23 20:00:45 -0700

    adds edit button and modal to each task, but doesn't update task yet

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -43,13 +43,20 @@ export function Tasks() {
 
   const removeTask = (task) => {
     console.log(task);
-
     axios.delete(`http://localhost:3000/tasks/${task.id}.json`).then(handleViewTasks);
   };
-  const updateTask = (task) => {
+  const updateTask = (id, params) => {
     console.log(task);
-    const request = { ...task, title: true };
-    axios.patch(`http://localhost:3000/tasks/${task.id}.json`).then(handleViewTasks);
+    axios.patch(`http://localhost:3000/tasks/${id}.json`, params).then((response) => {
+      setTasks(response.data);
+    });
+  };
+
+  const handleUpdate = (task) => {
+    task.preventDefault();
+    const params = new FormData(task.target);
+    updateTask(task.id, params);
+    handleViewTasks;
   };
 
   useEffect(() => handleViewTasks, [getTasks]);
@@ -120,8 +127,54 @@ export function Tasks() {
                 <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
               </svg>
             </button>
+            <button data-bs-toggle="modal" data-bs-target="#taskEditModal">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-pencil-square"
+                viewBox="0 0 16 16"
+              >
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+                />
+              </svg>
+            </button>
           </li>
         ))}
+      </div>
+      {/* Edit Task Modal */}
+      <div class="modal fade" id="taskEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Edit Task{" "}
+              </h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              <form onSubmit={handleUpdate}>
+                <h6>
+                  <input defaultValue={tasks?.title} name="title" type="text">
+                    {tasks?.title}
+                  </input>
+                </h6>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                    Close
+                  </button>
+                  <button type="submit" class="btn btn-primary">
+                    Update Task
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
