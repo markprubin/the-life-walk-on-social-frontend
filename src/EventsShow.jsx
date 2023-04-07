99b08597226539ab59eacdf435edfb6b5c786553
markprubin <mark.p.rubin@gmail.com>
2023-04-07 13:32:11 -0700

    updates Events show layout

--- src/EventsShow.jsx
+++ src/EventsShow.jsx
@@ -15,32 +15,33 @@ export function EventsShow(props) {
   const admin = localStorage.getItem("admin");
   return (
     <div>
-      <h1>Event information</h1>
-      <h2>{props.event.name}</h2>
-      <p>Description: {props.event.description}</p>
-      <p>Address: {props.event.address}</p>
-      <img src={props.event.image_url} width="300px" height="auto" />
-      <p>Start: {props.event.start_time} </p>
-      <p>End: {props.event.end_time} </p>
+      <div class="container text-center pb-4">
+        <h2 class="pb-4">{props.event.name}</h2>
+        <h5 class="pb-3">{props.event.description}</h5>
+        <img class="pb-3" src={props.event.image_url} width="300px" height="auto" />
+        <h5 class="pb-3">Address: {props.event.address}</h5>
+        <h6>Start: {props.event.start_time} </h6>
+        <h6 class="pb-3">End: {props.event.end_time} </h6>
+      </div>
       {admin === "true" ? (
         <form onSubmit={handleSubmit}>
-          <div>
+          <div class="mb-3">
             Name: <input defaultValue={props.event.name} name="name" type="text" />
           </div>
-          <div>
-            Description: <input defaultValue={props.event.description} name="description" type="text" />
+          <div class="mb-3">
+            Description: <textarea defaultValue={props.event.description} name="description" type="text" />
           </div>
-          <div>
+          <div class="mb-3">
             Address: <input defaultValue={props.event.address} name="address" type="text" />
           </div>
-          <div>
+          <div class="mb-3">
             Image Link: <input defaultValue={props.event.image_url} name="image_url" type="text" />
           </div>
-          <div>
+          <div class="mb-3">
             Start of Event:
             <input defaultValue={props.event.start_time} name="start_time" id="datetime" type="datetime-local" />
           </div>
-          <div>
+          <div class="mb-3">
             End of Event: <input defaultValue={props.event.end_time} name="end_time" type="datetime-local" />
           </div>
           <button type="submit">Update Event</button>
@@ -53,9 +54,6 @@ export function EventsShow(props) {
       )}
       <button onClick={handleClickFavorite} type="submit">
         Add to Favorites
-      </button>
-      <button onClick={handleRemoveFavorite} type="submit">
-        Remove Favorite
       </button>
     </div>
   );
