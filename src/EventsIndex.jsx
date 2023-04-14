ba7b4daceeb42034a966a7fa475d28ba111e7e93
markprubin <mark.p.rubin@gmail.com>
2023-04-13 23:06:05 -0700

    updates eventsindex styling

--- src/EventsIndex.jsx
+++ src/EventsIndex.jsx
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useRef } from "react";
+
+import "./App.css";
 
 export function EventsIndex(props) {
+  // Scroll to Events
+  const ref = useRef(null);
+  const doClick = () => ref.current?.scrollIntoView({ behavior: "smooth" });
   return (
+    // Top Feature
     <div>
       <div class="p-4 p-md-5 mb-4 rounded text-bg-dark">
         <div class="row justify-content-between">
@@ -22,7 +27,117 @@ export function EventsIndex(props) {
           </div>
         </div>
       </div>
-      <h1 className="text-center pb-5">Future Events</h1>
+      {/* Features */}
+      <div class="container px-4 py-5">
+        <h2 class="pb-2 border-bottom">What is The Life Walk-On Social?</h2>
+
+        <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+          <div class="col d-flex flex-column align-items-start gap-2">
+            <h3 class="fw-bold">A community of former athletes helping each other make it in a post-athletic life.</h3>
+            <p class="text-muted">
+              We want to bring together former athletes and create a community based on shared experiences from our
+              athletic past and use them to transition into a post-athletic life
+            </p>
+            <button onClick={doClick} class="btn btn-primary btn-lg container">
+              Browse Events
+            </button>
+          </div>
+
+          <div class="col">
+            <div class="row row-cols-1 row-cols-sm-2 g-4">
+              <div class="col d-flex flex-column gap-2">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    fill="currentColor"
+                    class="bi bi-calendar2-event"
+                    viewBox="0 0 16 16 "
+                  >
+                    <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                    <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z" />
+                    <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4z" />
+                  </svg>
+                </div>
+
+                <h4 class="fw-semibold mb-0">Events</h4>
+                <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+              </div>
+
+              <div class="col d-flex flex-column gap-2">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    fill="currentColor"
+                    class="bi bi-list-task"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M2 2.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5V3a.5.5 0 0 0-.5-.5H2zM3 3H2v1h1V3z"
+                    />
+                    <path d="M5 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM5.5 7a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 4a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M1.5 7a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5V7zM2 7h1v1H2V7zm0 3.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5H2zm1 .5H2v1h1v-1z"
+                    />
+                  </svg>
+                </div>
+                <h4 class="fw-semibold mb-0">Task list</h4>
+                <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+              </div>
+
+              <div class="col d-flex flex-column gap-2">
+                <div>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="50"
+                    height="50"
+                    fill="currentColor"
+                    class="bi bi-person-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                    />
+                  </svg>
+                </div>
+                <h4 class="fw-semibold mb-0">Profiles</h4>
+                <p class="text-muted">Paragraph of text beneath the heading to explain the heading.</p>
+              </div>
+
+              <div class="col d-flex flex-column gap-2">
+                <div class="feature-icon-small d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-4 rounded-3"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div>
+          <h3 class="fw-bold">What are we looking to add in the future?</h3>
+          <p class="text-muted mx-6 py-2">
+            We want to bring together former athletes and create a community based on shared experiences from our
+            athletic past and use them to transition into a post-athletic life
+          </p>
+          <ul>
+            <h4 class="pb-2">Community Forum</h4>
+            <h4 class="pb-2">Chat and Connect</h4>
+            <h4 class="pb-2">Comments in Events</h4>
+          </ul>
+        </div>
+      </div>
+      <hr class="solid py-1" />
+
+      {/* All events */}
+      <h1 ref={ref} className="text-center pb-5">
+        Future Events
+      </h1>
       <div class="container text-center">
         <div class="row">
           {props.events.map((event) => (
--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -3,22 +3,32 @@ import React from "react";
 export function ListItem(props) {
   if (props.style === "favorite") {
     return (
-      <li key={props.title}>
+      <ul key={props.title}>
         <h4>{props.title}</h4>
         <h5>{props.date}</h5>
-        <button onClick={() => props.handleDestroyFavorite(props.favorite)}>
+        <h5>{props.start_time}</h5>
+
+        <button
+          onClick={() => {
+            if (window.confirm("Are you sure you want to delete this favorite?")) {
+              props.handleDestroyFavorite(props.favorite);
+            }
+          }}
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width="25"
+            height="25"
             fill="currentColor"
-            class="bi bi-x-square-fill"
+            class="bi bi-trash"
             viewBox="0 0 16 16"
           >
-            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z" />
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
           </svg>
         </button>
-      </li>
+        <hr />
+      </ul>
     );
   }
   return (
