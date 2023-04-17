1f1921b0554f5a6dacd85fc150029366b6877a99
markprubin <mark.p.rubin@gmail.com>
2023-04-17 14:45:33 -0700

    updates header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,6 @@ import { Login } from "./Login";
 import { useState, useEffect } from "react";
 import { Signup } from "./Signup";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export function Header() {
   const [user, setUser] = useState([]);
@@ -21,11 +20,8 @@ export function Header() {
     <header class="p-3 mb-3 border-bottom shadow">
       <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"></svg>
-          </a>
-
-          <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+          <img class="d-block mx-4 mb-2 col-2" src="src/assets/TLWO Short.png" alt="" />
+          <ul class="nav col-12 col-lg-auto mx-6 me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
               <a href="/home" class="nav-link px-2 link-dark">
                 Home
@@ -62,10 +58,10 @@ export function Header() {
               </div>
             )}
           </ul>
-
+          {/* 
           <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
             <input type="search" class="form-control" placeholder="Search..." aria-label="Search" />
-          </form>
+          </form> */}
           {localStorage.jwt === undefined ? (
             <></>
           ) : (
