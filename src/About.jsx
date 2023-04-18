294bd92639068ea722ed26b913da38e8b7194ff0
markprubin <mark.p.rubin@gmail.com>
2023-04-18 10:20:41 -0700

    updates about section

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,27 @@
 export function About() {
   return (
     <div>
-      <h1>You got the gist on the welcome page!</h1>
+      <h1 className="my-5">Welcome!</h1>
+      <h2>About The Founder</h2>
+      <p>
+        Mark Rubin is an independent personal trainer in Phoenix and Scottsdale, where he strives to work with any
+        client of any level to help them put the pieces together towards fulfilling their potential. ​ Mark was a
+        collegiate swimmer at the University of the Incarnate Word, where he also obtained his Bachelor of Science in
+        Biology. After deciding that dental school was not what he wanted to do, he left two months into the first
+        semester to pursue a career as a coach. He received his Master of Science in Exercise and Wellness at Arizona
+        State University.
+      </p>
+      <p>
+        After an extensive career in sports performance and strength and conditioning, working with soccer clubs like
+        Norwich City FC, San Antonio FC, as well as working with youth athletes in the middle school, high school, and
+        collegiate level, he explored some hobbies outside of work, and came across coding.
+      </p>
+      <p>
+        What became a love for problem solving and logic turned into a potential career transition... again! After
+        completing the Actualize Coding Bootcamp, he is continuing practicing coding and looking to break into the tech
+        world. He vowed to have a positive effect on this world if he transitioned careers, and that is what he aims to
+        do.
+      </p>
     </div>
   );
 }
